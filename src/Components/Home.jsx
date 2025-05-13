72f704841778647585649b41750dab4c285ba7bf
Amyy0404 <anyvv9@gmail.com>
2025-05-13 10:27:05 +0200

    images

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -30,33 +30,29 @@ function Home() {
   </div>
 </div>
 
-      <div className="feature-buttons">
-        <div className="feature-card" onClick={() => navigate("/search")}>
-          <div className="icon-circle">
-          </div>
-          <h3>SEARCH</h3>
-          <p>Looking for something specific?</p>
-        </div>
-        <div className="feature-card" onClick={() => navigate("/discover")}>
-          <div className="icon-circle">
-          </div>
-          <h3>DISCOVER</h3>
-          <p>Farmstalls, restaurants, One-Stops, Mechanics and more...</p>
-        </div>
-        <div className="feature-card" onClick={() => navigate("/deadzones")}>
-          <div className="icon-circle">
-          </div>
-          <h3>DEADZONES</h3>
-          <p>Know when there is no signal</p>
-        </div>
-        <div className="feature-card" onClick={() => navigate("/helplines")}>
-          <div className="icon-circle">
-          </div>
-          <h3>HELPLINES</h3>
-          <p>Emergency numbers for each province</p>
-        </div>
-      </div>
-    </div>
+<div className="feature-buttons">
+  <div className="feature-card" onClick={() => navigate("/search")}>
+    <div className="icon-circle icon-search"></div>
+    <h3>SEARCH</h3>
+    <p>Looking for something specific?</p>
+  </div>
+  <div className="feature-card" onClick={() => navigate("/discover")}>
+    <div className="icon-circle icon-discover"></div>
+    <h3>DISCOVER</h3>
+    <p>Farmstalls, restaurants, One-Stops, Mechanics and more...</p>
+  </div>
+  <div className="feature-card" onClick={() => navigate("/deadzones")}>
+    <div className="icon-circle icon-deadzones"></div>
+    <h3>DEADZONES</h3>
+    <p>Know when there is no signal</p>
+  </div>
+  <div className="feature-card" onClick={() => navigate("/helplines")}>
+    <div className="icon-circle icon-helplines"></div>
+    <h3>HELPLINES</h3>
+    <p>Emergency numbers for each province</p>
+  </div>
+</div>
+</div>
   );
 }
 
