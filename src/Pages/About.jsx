bda2ae2d1bda1636e8d5bd26c9bfb02728fc7ac5
Amyy0404 <anyvv9@gmail.com>
2025-05-14 10:36:24 +0200

    test

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import "../Styles/About.css";
 
 function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>This is where users can search for specific places or services.</p>
+    <div className="about-page">
+
+      <div className="hero-section">
+        <h1>About Us</h1>
+        <p className="quote">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
+      </div>
+
+      <div className="image-row">
+        <div className="image-box box1"></div>
+        <div className="image-box box2"></div>
+        <div className="image-box box3"></div>
+        <div className="image-box box4"></div>
+      </div>
+
+      <div className="story-section">
+        <h2>Our Story...</h2>
+        <div className="story-columns">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
+        </div>
+      </div>
     </div>
   );
 }
