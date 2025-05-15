71193a8422d36d4ddf7ced13568134cf26a259cd
Amyy0404 <anyvv9@gmail.com>
2025-05-15 18:54:07 +0200

    idek

--- src/Data/Places.js
+++ src/Data/Places.js
@@ -7,7 +7,7 @@ const places = [
     tags: ["Attraction", "Hidden Gem"],
     description: "Food, gifts, instagrammable area, niche",
     rating: 4.5,
-    image: "../Public/Images/Data/Farmstall1.png"
+    image: "../Images/Farmstall1.png"
   },
   {
     id: 2,
@@ -17,7 +17,7 @@ const places = [
     tags: ["Open 24/7", "Point of Interest"],
     description: "Great stop for late-night refuel and coffee.",
     rating: 4.2,
-    image: "../Images/Data/Garage1.png"
+    image: "../Images/Garage1.png"
   },
   {
     id: 3,
@@ -27,7 +27,7 @@ const places = [
     tags: ["Hidden Gem"],
     description: "Reliable roadside mechanic known for fair pricing.",
     rating: 4.7,
-    image: "../Images/Data/Mechanic1.png"
+    image: "../Imagess/Mechanic1.png"
   }
 ];
 
