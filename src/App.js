aa9d55ec01a7ac7ec72386431e7dc1cb62b091cb
Amyy0404 <anyvv9@gmail.com>
2025-05-27 21:15:09 +0200

    okayy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import {  BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import Login from "./Pages/Login";
