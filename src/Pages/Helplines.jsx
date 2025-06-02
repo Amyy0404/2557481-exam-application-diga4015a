2b2d48a7b668b63ff0d1628e1d935172733ab166
Amyy0404 <anyvv9@gmail.com>
2025-06-02 15:13:17 +0200

    Photos

--- src/Pages/Helplines.jsx
+++ src/Pages/Helplines.jsx
@@ -1,10 +1,46 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../Styles/Helplines.css";
+
+const provinces = [
+  { name: "Gauteng", image: "gauteng.png", hoverText: "Help in Gauteng" },
+  { name: "Western Cape", image: "westerncape.png", hoverText: "Help in Western Cape" },
+  { name: "KwaZulu-Natal", image: "kzn.png", hoverText: "Help in KZN" },
+  { name: "Eastern Cape", image: "easterncape.png", hoverText: "Help in Eastern Cape" },
+  { name: "Free State", image: "freestate.png", hoverText: "Help in Free State" },
+  { name: "Limpopo", image: "limpopo.png", hoverText: "Help in Limpopo" },
+  { name: "Mpumalanga", image: "mpumalanga.png", hoverText: "Help in Mpumalanga" },
+  { name: "North West", image: "northwest.png", hoverText: "Help in North West" },
+  { name: "Northern Cape", image: "northerncape.png", hoverText: "Help in Northern Cape" },
+];
 
 function Helplines() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Helplines Page</h1>
-      <p>This is where users can search for specific places or services.</p>
+    <div className="helplines-page">
+      <h1 className="helplines-heading">HELPLINES</h1>
+      <p className="helplines-subheading">
+        Select what province you are in and find the nearest help
+      </p>
+
+      <div className="province-grid">
+        {provinces.map((province, index) => (
+          <div
+            key={index}
+            className="province-button"
+            onClick={() => navigate("/HelplinesMap")}
+          >
+            <img
+              src={require(`../Images/Provinces/${province.image}`)}
+              alt={province.name}
+              className="province-icon"
+            />
+            <span className="province-name">{province.name}</span>
+            <div className="hover-text">{province.hoverText}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/Pages/HelplinesMap.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HelplinesMap() {
+  return (
+    <div>
+      <h1>Helplines Page</h1>
+      <p>This is where users can search for specific places or services.</p>
+    </div>
+  );
+}
+
+export default HelplinesMap;
